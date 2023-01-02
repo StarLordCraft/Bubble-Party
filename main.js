56d374d7b23323de7fff4545ea8263c15215bbad
StarLordCraft <rafa.viana.moc@gmail.com>
2023-01-02 17:56:42 -0300

    Remaking the HTML and CSS - mobile first and accessibility issues fixed

---
+++ main.js
@@ -0,0 +1,5 @@
+const palco = document.getElementById('palco');
+const NumObjetos = document.getElementById('numObjects');
+const addBubble = document.getElementById('numBubblesAdd');
+const btnAdicionar = document.getElementById('btnAdicionar');
+const btnRemover = document.getElementById('btnRemover');
